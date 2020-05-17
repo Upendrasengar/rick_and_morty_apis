29e87790b07fe4f70ca7d4cb6b2ae1235cc31d28
Upendra Sengar <upendra.sengar@publicissapient.com>
2020-05-17 18:05:36 +0530

    Build prod

---
+++ app.js
@@ -0,0 +1,19 @@
+
+var express = require('express');
+
+var path = require('path');
+
+var app = express();
+
+app.use(express.static('web-app'))
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/web-app/index.html'));
+});
+
+module.exports = app;
