ee82b150c3585e930bd9a7966cee301cb79100f4
Upendra Sengar <upendra.sengar@publicissapient.com>
2020-05-17 18:22:42 +0530

    Name change

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('rmCastApp app is running!');
+    expect(page.getTitleText()).toEqual('rickAndMortyApps app is running!');
   });
 
   afterEach(async () => {
